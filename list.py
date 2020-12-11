9ec40c5bd986eeaab604e4496ce72cb56c9f9a29
Gustav <sir.gustav.the.coder@gmail.com>
2020-12-11 16:22:33 +0100

    code cleanup

--- list.py
+++ list.py
@@ -2,21 +2,48 @@
 import os
 import subprocess
 
-d = '.'
-dirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
-gd = []
+def all_folders_in(d):
+    return [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
 
-for dd in dirs:
-    gf = os.path.join(dd, '.git')
-    if os.path.isdir(gf):
-        remote = subprocess.check_output(["git", "remote", '-v'], cwd=dd, universal_newlines=True)
-        print(dd)
-        print(remote)
+
+class Gitrepo:
+    def __init__(self, folder, repo):
+        self.folder = folder
+        self.repo = repo
+
+
+def is_folder_a_git_repo(dd):
+    return os.path.isdir(os.path.join(dd, '.git'))
+
+
+class Program:
+    gits = []
+    nons = []
+
+    def run(self, root):
+        dirs = all_folders_in(root)
+        for dd in dirs:
+            if is_folder_a_git_repo(dd):
+                remote = subprocess.check_output(["git", "remote", '-v'], cwd=dd, universal_newlines=True)
+                self.gits.append(Gitrepo(dd, remote))
+            else:
+                self.nons.append(dd)
+
+    def report(self):
+        for f in self.gits:
+            print(f.folder)
+            print(f.repo)
+            print()
         print()
-    else:
-        gd.append(dd)
+        print('non git:')
+        for f in self.nons:
+            print(f)
 
-print()
-print('non git:')
-for dd in gd:
-    print(dd)
+
+def main():
+    p = Program()
+    p.run(os.getcwd())
+    p.report()
+
+if __name__ == "__main__":
+    main()
